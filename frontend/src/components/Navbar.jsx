78d77be0584f04d8cba817d5b1d78c2929473f76
user name <user email>
2024-02-16 16:49:55 +0530

    Removed Horizontal break in Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -18,8 +18,6 @@ export default function NavBar({ onNavClick }) {
         </Link>
       </nav>
 
-      <hr />
-
       {/* An <Outlet> renders whatever child route is currently active,
           so you can think about this <Outlet> as a placeholder for
           the child routes we defined above. */}
